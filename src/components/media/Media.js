c0827879904fb1d2c41396dd24cea0e05789d091
madison-hepner <hepnermadison@icloud.com>
2022-04-08 21:00:57 -0500

    media delete button

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,12 +1,15 @@
 import react from "react";
 import { Link } from "react-router-dom";
 // import './ArticleCard.css'
-import './../TopSpeed.css'
-import { useNavigate } from "react-router-dom";
+// import './../TopSpeed.css'
+import { useNavigate, useParams } from "react-router-dom";
 import "./Media.css"
 
 
-export const MediaCard = ({ media }) => {
+export const MediaCard = ({ media, handleDeleteMedia }) => {
+    const sessionUser = JSON.parse(window.sessionStorage.getItem("topspeed_user"))
+    const sessionUserId = sessionUser.id
+    const {postId} = useParams();
 
     const navigate = useNavigate();
 
@@ -22,6 +25,19 @@ export const MediaCard = ({ media }) => {
             <div className="media__overlay">
                     <p className="media__caption">{media.caption}</p>
                     <p className="media__user"><strong>Posted by:</strong>{media?.user.name}</p>
+                    { media?.userId === sessionUserId
+        ? <>
+        <section className="media__container">
+        <div className="media__delete">
+            <div className="media__delete__btns">
+                <button type="button" className="media__delete__btns__btn" id="media__delete__btn" onClick={() => handleDeleteMedia(media.id)} ><small>delete post</small></button>
+            </div>
+        </div>
+        </section>
+        </>
+        : ""
+        }
+                    
             </div>
             </div>
             </div>
